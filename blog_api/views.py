4a1a5c81c8132e12aa6aaab920bd6604139153a7
mukit24 <mukithasanpranto4@gmail.com>
2022-11-24 23:37:30 +0600

    permissions

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,12 +1,24 @@
 from rest_framework import generics
 from .serializers import PostSerializer
 from blog.models import Post
+from rest_framework.permissions import IsAuthenticatedOrReadOnly,BasePermission, SAFE_METHODS
+
+class PostUserPermission(BasePermission):
+    message = 'This operation is restricted only to author'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        else:
+            return obj.author == request.user
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
     
 
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView,PostUserPermission):
+    permission_classes = [PostUserPermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
